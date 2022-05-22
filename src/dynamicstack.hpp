a39c7b424974275b4d67a9746be2c1113cfc6fb9
farinellizin <lucasfcpinho@gmail.com>
2022-05-22 16:34:05 -0300

    Stack Problems V0.0: initializing repository and preparing files.

--- src/dynamicstack.hpp
+++ src/dynamicstack.hpp
@@ -1,4 +1,30 @@
 #ifndef DYNAMICSTACK_HPP
 #define DYNAMICSTACK_HPP
+#include <iostream>
+#include <time.h>
+#include <unistd.h>
+
+typedef struct Item Item;
+typedef struct Block Block;
+typedef struct Pilha Pilha;
+
+struct Item{
+	int val;
+};
+
+struct Block{
+	Item data;
+	Block *prox;
+};
+
+struct Pilha{
+	Block *base;
+	Block *top;
+};
+
+void FPVazia(Pilha *p);
+void Push(Pilha *p, Item d);
+void Pop(Pilha *p, Item *d);
+void PImprime(Pilha *p);
 
 #endif
--- src/linearstack.hpp
+++ src/linearstack.hpp
@@ -2,7 +2,25 @@
 #define LINEARSTACK_HPP
 using namespace std;
 #include <iostream>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
+
+#define MAXTAM 10
+
+typedef struct Item{
+	int val;
+} Item;
+
+typedef struct Pilha{
+	Item vet[MAXTAM];
+	int top;
+	int base;
+} Pilha;
+
+void FPVazia(Pilha *p);
+void PUSH(Pilha *p, Item d);
+void POP(Pilha *p, Item *d);
+void PRemove(Pilha *p, Item d);
+void PImprime(Pilha *p);
 
 #endif
